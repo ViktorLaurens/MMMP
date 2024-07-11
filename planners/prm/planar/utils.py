8be12dd665092b30bb17b6e20c1ea628795f30a0
viktor <viktor.degroote@gmail.com>
2024-07-11 12:22:36 +0200

    Added utils file for PRM utilities in planar environment

---
+++ planners/prm/planar/utils.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, id, q):
+        self.id = id
+        self.q = tuple([round(c, 2) for c in q])
+    def __eq__(self, other):
+        return self.id == other.id
+    def __hash__(self):
+        return hash(str(self.id) + str(self.q))
+    def __str__(self):
+        return str(self.id, self.q)    
