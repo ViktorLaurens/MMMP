473eb968957a4cb83c65bbd8424bd877ab608de3
viktor <viktor.degroote@gmail.com>
2024-08-27 20:31:58 +0200

    Added execute motion functions while frames are being captured

--- planners/prm/pybullet/env.py
+++ planners/prm/pybullet/env.py
@@ -162,6 +162,43 @@ class Environment:
                 p.stepSimulation()
                 time.sleep(time_step)
         return trajectories     
+    
+    def execute_interpolated_motion_capturing_frames(self, prm, id_paths, t_final):
+        # # Get waypoints from the paths
+        # waypoints = {r_id: [] for r_id in id_paths.keys()}
+        # for r_id, path in id_paths.items():
+        #     for node_id in path:
+        #         waypoints[r_id].append(np.array(prm.node_id_q_dicts[prm.r_ids.index(r_id)][node_id]))
+        
+        # calculate interpolated trajectories
+        trajectories = {r_id: [] for r_id in id_paths.keys()}
+        for r_id in id_paths.keys():
+            waypoints = [np.array(prm.node_id_q_dicts[prm.r_ids.index(r_id)][node_id]) for node_id in id_paths[r_id]]
+            norms = np.linalg.norm(np.diff(waypoints, axis=0), axis=1)
+            total_norm = np.sum(norms)
+            times = np.cumsum(np.concatenate(([0], norms / total_norm))) * t_final
+            for i, t in enumerate(times):
+                trajectories[r_id].append((t, waypoints[i]))
+
+        # Simulation parameters
+        time_step = 1.0 / 30.0
+        p.setTimeStep(time_step)
+
+        # Loop through trajectory points
+        t_start = 0.0
+        t_stop, _ = trajectories[prm.r_ids[0]][-1]
+        frames = []
+        for t in np.linspace(t_start, t_stop, num=int((t_stop - t_start) / time_step)):
+            for r_id in prm.r_ids:
+                joint_positions = self.q_from_interpolated_trajectories(trajectories[r_id], t)   
+                if len(joint_positions) != 7:
+                    continue                     
+                p.setJointMotorControlArray(r_id, range(7), p.POSITION_CONTROL, joint_positions)
+                p.stepSimulation()
+                time.sleep(time_step)
+                img = self.capture_frame()
+                frames.append(img)
+        return frames, trajectories
 
     def q_from_interpolated_trajectories(self, trajectories, t):
         joint_positions = []
@@ -214,3 +251,44 @@ class Environment:
                 p.stepSimulation()
                 time.sleep(time_step)
         return trajectories
+    
+    def execute_smooth_motion_capturing_frames(self, prm, id_paths, t_final=2.0, effort_factor=0.8):
+        # Get waypoints from the paths
+        waypoints = {r_id: [] for r_id in id_paths.keys()}
+        for r_id, path in id_paths.items():
+            for node in path:
+                q = np.array(prm.node_id_q_dicts[prm.r_ids.index(r_id)][node])
+                waypoints[r_id].append(q)
+
+        # Joint limits for the Panda robot
+        joint_limits = {
+            'q_max': [2.7437, 1.7837, 2.9007, -0.1518, 2.8065, 4.5169, 3.0159],
+            'q_min': [-2.7437, -1.7837, -2.9007, -3.0421, -2.8065, 0.5445, -3.0159]
+        }
+        
+        # Velocity and acceleration limits
+        velocity_limits = np.array([2.62, 2.62, 2.62, 2.62, 5.26, 4.18, 5.26])  # rad/s
+        acceleration_limits = np.array([10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0]) # rad/s²
+
+        # LSPB trajectories
+        trajectories = {r_id: calculate_lspb_trajectory(waypoints[r_id], t_final, effort_factor, joint_limits, velocity_limits, acceleration_limits) for r_id in id_paths.keys()}
+
+        # Simulation parameters
+        time_step = 1.0 / 30.0
+        p.setTimeStep(time_step)
+
+        # Loop through trajectory points
+        t_start = 0.0
+        t_stop = trajectories[prm.r_ids[0]][0][-1].t2
+        frames = []
+        for t in np.linspace(t_start, t_stop, num=int((t_stop - t_start) / time_step)):
+            for r_id in prm.r_ids:
+                joint_positions = joint_positions_from_trajectories(trajectories[r_id], t)
+                if len(joint_positions) != 7:
+                    continue
+                p.setJointMotorControlArray(r_id, range(7), p.POSITION_CONTROL, joint_positions)
+                p.stepSimulation()
+                time.sleep(time_step)
+                img = self.capture_frame()
+                frames.append(img)
+        return frames, trajectories
