9363b96941ff1eafdeda76a2269d476125236b4e
viktor <viktor.degroote@gmail.com>
2024-08-28 13:44:58 +0200

    Added functions for saving videos and GIFs.

--- utils/pb_data_utils.py
+++ utils/pb_data_utils.py
@@ -7,6 +7,8 @@ This module contains functions for processing or modifying data.
 import os
 import re
 
+import numpy as np
+
 
 def safe_zip(sequence1, sequence2):
     sequence1, sequence2 = list(sequence1), list(sequence2)
@@ -48,3 +50,42 @@ def load_roadmap(filename):
             roadmap[node] = neighbors
 
     return roadmap
+
+# Video and GIF creation
+def save_video(frames, directory, filename, fps=30):
+    """
+    Saves a list of frames as a video file.
+
+    Args:
+    - frames (list): A list of frames to save as a video.
+    - directory (str): The directory to save the video file.
+    - filename (str): The name of the video file to save.
+    - fps (int, optional): The frames per second of the video. Defaults to 30.
+    """
+    import imageio
+
+    # Ensure frames are in a correct format
+    frames = [np.uint8(frame) for frame in frames]  # Convert each frame to uint8 if necessary
+
+    # Save the captured frames as a video
+    imageio.mimsave(os.path.join(directory, filename), frames, fps=fps)  # Define the FPS as needed
+    print(f"Video saved as {os.path.join(directory, filename)}")
+
+def save_gif(frames, directory, filename, duration=33):
+    """
+    Saves a list of frames as a GIF file.
+
+    Args:
+    - frames (list): A list of frames to save as a GIF.
+    - directory (str): The directory to save the GIF file.
+    - filename (str): The name of the GIF file to save.
+    - duration (int, optional): The duration of each frame in milliseconds. Defaults to 33.
+    """
+    import imageio
+
+    # Ensure frames are in a correct format
+    frames = [np.uint8(frame) for frame in frames]  # Convert each frame to uint8 if necessary
+
+    # Save the captured frames as a GIF
+    imageio.mimsave(os.path.join(directory, filename), frames, duration=duration, loop=0)  # Define the duration or fps as needed
+    print(f"GIF saved as {os.path.join(directory, filename)}")
